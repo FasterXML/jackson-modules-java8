582e142f6f6f8ae84ca4c52892a2615c6111122d
lpandzic <lovro.pandzic@infobip.com>
2017-05-24 20:00:18 +0200

    added test for new feature in #1631

--- parameter-names/src/test/java/com/fasterxml/jackson/module/paramnames/JsonCreatorTest.java
+++ parameter-names/src/test/java/com/fasterxml/jackson/module/paramnames/JsonCreatorTest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.databind.*;
 import org.junit.*;
 
+import java.io.IOException;
+
 import static org.assertj.core.api.BDDAssertions.*;
 
 public class JsonCreatorTest
@@ -23,6 +25,21 @@ public class JsonCreatorTest
 		then(actual).isEqualToComparingFieldByField(new ClassWithJsonCreatorOnStaticMethod("1st", "2nd"));
 	}
 
+	@Test
+	public void shouldDeserializeUsingDefaultPropertyCreatorSetting() throws IOException {
+		// given
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new ParameterNamesModule());
+		objectMapper.configure(MapperFeature.SET_PROPERTY_CREATOR_AS_DEFAULT, true);
+		int givenValue = 1;
+
+		// when
+		SinglePropertyValueClass actual = objectMapper.readValue("{\"value\":\"" + givenValue + "\"}",
+		                                                         SinglePropertyValueClass.class);
+		// then
+        then(actual).isEqualToComparingFieldByField(new SinglePropertyValueClass(givenValue));
+	}
+
 	static class ClassWithJsonCreatorOnStaticMethod {
 		final String first;
 		final String second;
@@ -36,6 +53,18 @@ public class JsonCreatorTest
 		static ClassWithJsonCreatorOnStaticMethod factory(String first, String second) {
 
 			return new ClassWithJsonCreatorOnStaticMethod(first, second);
+		}
+	}
+
+	static class SinglePropertyValueClass {
+		private final Integer value;
+
+		SinglePropertyValueClass(Integer value) {
+			this.value = value;
+		}
+
+		public Integer getValue() {
+			return value;
 		}
 	}
 }
