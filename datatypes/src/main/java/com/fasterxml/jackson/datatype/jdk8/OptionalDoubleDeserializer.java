16b391b999725d0b9c768ad176dcbf686a8f71d3
Carter Kozak <ckozak@ckozak.net>
2020-11-19 13:21:08 -0500

    Fix OptionalDouble deserialization of special values without coercion (#195)
    floating point special values as JSON Strings are considered "native"
    because they cannot be represented as JSON numbers.
    
    See:
    https://github.com/FasterXML/jackson-databind/pull/2942
    https://github.com/FasterXML/jackson-databind/commit/7705d35fc0f58e5aec389b2cdadf9a046a55b00b

--- datatypes/src/main/java/com/fasterxml/jackson/datatype/jdk8/OptionalDoubleDeserializer.java
+++ datatypes/src/main/java/com/fasterxml/jackson/datatype/jdk8/OptionalDoubleDeserializer.java
@@ -32,6 +32,14 @@ class OptionalDoubleDeserializer extends BaseScalarOptionalDeserializer<Optional
         switch (p.currentTokenId()) {
         case JsonTokenId.ID_STRING:
             String text = p.getText();
+            // 19-Nov-2020, ckozak: see jackson-databind#2942: Special case, floating point special
+            //     values as String (e.g. "NaN", "Infinity", "-Infinity" need to be considered
+            //     "native" representation as JSON does not allow as numbers, and hence not bound
+            //     by coercion rules
+            Double specialValue = _checkDoubleNaN(text);
+            if (specialValue != null) {
+                return OptionalDouble.of(specialValue);
+            }
             CoercionAction act = _checkFromStringCoercion(ctxt, text);
             // null and empty both same
             if ((act == CoercionAction.AsNull) || (act == CoercionAction.AsEmpty)) {
