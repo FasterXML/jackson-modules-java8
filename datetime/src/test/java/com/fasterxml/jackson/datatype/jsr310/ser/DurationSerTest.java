646b6fe3de35af8688cbb8ede74c0631962cab98
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-10-29 17:04:31 -0700

    Minor test simplification

--- datetime/src/test/java/com/fasterxml/jackson/datatype/jsr310/ser/DurationSerTest.java
+++ datetime/src/test/java/com/fasterxml/jackson/datatype/jsr310/ser/DurationSerTest.java
@@ -1,5 +1,10 @@
 package com.fasterxml.jackson.datatype.jsr310.ser;
 
+import java.time.Duration;
+import java.time.temporal.TemporalAmount;
+
+import org.junit.Test;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,23 +13,11 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.MockObjectConfiguration;
 import com.fasterxml.jackson.datatype.jsr310.ModuleTestBase;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import java.time.Duration;
-import java.time.temporal.TemporalAmount;
-
 import static org.junit.Assert.*;
 
 public class DurationSerTest extends ModuleTestBase
 {
-    private ObjectWriter WRITER;
-
-    @Before
-    public void setUp()
-    {
-        WRITER = newMapper().writer();
-    }
+    private final ObjectWriter WRITER = newMapper().writer();
 
     // [datetime#224]
     static class MyDto224 {
@@ -56,7 +49,7 @@ public class DurationSerTest extends ModuleTestBase
                 .with(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS)
                 .with(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS)
                 .writeValueAsString(duration);
-        assertEquals("The value is not correct.", "60"+NO_NANOSECS_SUFFIX, value);
+        assertEquals("60"+NO_NANOSECS_SUFFIX, value);
     }
 
     @Test
@@ -67,7 +60,7 @@ public class DurationSerTest extends ModuleTestBase
                 .with(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS)
                 .with(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS)
                 .writeValueAsString(duration);
-        assertEquals("The value is not correct.", "13498.000008374", value);
+        assertEquals("13498.000008374", value);
     }
 
     // [modules-java8#165]
@@ -81,10 +74,10 @@ public class DurationSerTest extends ModuleTestBase
         // 20-Oct-2020, tatu: Very weird, but "use nanoseconds" actually results
         //   in unit being seconds, with fractions (with nanosec precision)
         String value = w.writeValueAsString(Duration.ofMillis(1L));
-        assertEquals("The value is not correct.", "0.001000000", value);
+        assertEquals("0.001000000", value);
 
         value = w.writeValueAsString(Duration.ofMillis(-1L));
-        assertEquals("The value is not correct.", "-0.001000000", value);
+        assertEquals("-0.001000000", value);
     }
     
     @Test
@@ -94,11 +87,11 @@ public class DurationSerTest extends ModuleTestBase
                 .with(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS)
                 .without(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
         String value = w.writeValueAsString(Duration.ofSeconds(45L, 0));
-        assertEquals("The value is not correct.", "45000", value);
+        assertEquals("45000", value);
 
         // and with negative value too
         value = w.writeValueAsString(Duration.ofSeconds(-32L, 0));
-        assertEquals("The value is not correct.", "-32000", value);
+        assertEquals("-32000", value);
     }
 
     @Test
@@ -108,7 +101,7 @@ public class DurationSerTest extends ModuleTestBase
                 .with(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS)
                 .without(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS)
                 .writeValueAsString(Duration.ofSeconds(13498L, 8374));
-        assertEquals("The value is not correct.", "13498000", value);
+        assertEquals("13498000", value);
     }
 
     @Test
@@ -119,7 +112,7 @@ public class DurationSerTest extends ModuleTestBase
                 .with(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS)
                 .without(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS)
                 .writeValueAsString(duration);
-        assertEquals("The value is not correct.", "13498837", value);
+        assertEquals("13498837", value);
     }
 
     @Test
@@ -129,7 +122,7 @@ public class DurationSerTest extends ModuleTestBase
         String value = WRITER
                 .without(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS)
                 .writeValueAsString(duration);
-        assertEquals("The value is not correct.", '"' + duration.toString() + '"', value);
+        assertEquals(q(duration.toString()), value);
     }
 
     @Test
@@ -139,7 +132,7 @@ public class DurationSerTest extends ModuleTestBase
         String value = WRITER
                 .without(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS)
                 .writeValueAsString(duration);
-        assertEquals("The value is not correct.", '"' + duration.toString() + '"', value);
+        assertEquals(q(duration.toString()), value);
     }
 
     @Test
@@ -153,7 +146,7 @@ public class DurationSerTest extends ModuleTestBase
         Duration duration = Duration.ofSeconds(13498L, 8374);
         String value = mapper.writeValueAsString(duration);
 
-        assertEquals("The value is not correct.",
+        assertEquals(
                 "[\"" + Duration.class.getName() + "\",13498.000008374]", value);
     }
 
@@ -168,7 +161,7 @@ public class DurationSerTest extends ModuleTestBase
         Duration duration = Duration.ofSeconds(13498L, 837481723);
         String value = mapper.writeValueAsString(duration);
 
-        assertEquals("The value is not correct.",
+        assertEquals(
                 "[\"" + Duration.class.getName() + "\",13498837]", value);
     }
 
@@ -182,7 +175,7 @@ public class DurationSerTest extends ModuleTestBase
         Duration duration = Duration.ofSeconds(13498L, 8374);
         String value = mapper.writeValueAsString(duration);
 
-        assertEquals("The value is not correct.",
+        assertEquals(
                 "[\"" + Duration.class.getName() + "\",\"" + duration.toString() + "\"]", value);
     }
 
@@ -196,165 +189,105 @@ public class DurationSerTest extends ModuleTestBase
     public void shouldSerializeInNanos_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("NANOS");
-
-        Duration duration = Duration.ofHours(1);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "3600000000000", value);
+        assertEquals("3600000000000", mapper.writeValueAsString(Duration.ofHours(1)));
     }
 
     @Test
     public void shouldSerializeInMicros_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("MICROS");
-
-        Duration duration = Duration.ofMillis(1);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "1000", value);
+        assertEquals("1000", mapper.writeValueAsString(Duration.ofMillis(1)));
     }
 
     @Test
     public void shouldSerializeInMicrosDiscardingFractions_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("MICROS");
-
-        Duration duration = Duration.ofNanos(1500);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "1", value);
+        assertEquals("1", mapper.writeValueAsString(Duration.ofNanos(1500)));
     }
 
     @Test
     public void shouldSerializeInMillis_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("MILLIS");
-
-        Duration duration = Duration.ofSeconds(1);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "1000", value);
+        assertEquals("1000", mapper.writeValueAsString(Duration.ofSeconds(1)));
     }
 
     @Test
     public void shouldSerializeInMillisDiscardingFractions_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("MILLIS");
-
-        Duration duration = Duration.ofNanos(1500000);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "1", value);
+        assertEquals("1", mapper.writeValueAsString(Duration.ofNanos(1500000)));
     }
 
     @Test
     public void shouldSerializeInSeconds_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("SECONDS");
-
-        Duration duration = Duration.ofMinutes(1);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "60", value);
+        assertEquals("60", mapper.writeValueAsString(Duration.ofMinutes(1)));
     }
 
     @Test
     public void shouldSerializeInSecondsDiscardingFractions_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("SECONDS");
-
-        Duration duration = Duration.ofMillis(1500);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "1", value);
+        assertEquals("1", mapper.writeValueAsString(Duration.ofMillis(1500)));
     }
 
     @Test
     public void shouldSerializeInMinutes_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("MINUTES");
-
-        Duration duration = Duration.ofHours(1);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "60", value);
+        assertEquals("60", mapper.writeValueAsString(Duration.ofHours(1)));
     }
 
     @Test
     public void shouldSerializeInMinutesDiscardingFractions_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("MINUTES");
-
-        Duration duration = Duration.ofSeconds(90);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "1", value);
+        assertEquals("1", mapper.writeValueAsString(Duration.ofSeconds(90)));
     }
 
     @Test
     public void shouldSerializeInHours_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("HOURS");
-
-        Duration duration = Duration.ofDays(1);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "24", value);
+        assertEquals("24", mapper.writeValueAsString(Duration.ofDays(1)));
     }
 
     @Test
     public void shouldSerializeInHoursDiscardingFractions_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("HOURS");
-
-        Duration duration = Duration.ofMinutes(90);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "1", value);
+        assertEquals("1", mapper.writeValueAsString(Duration.ofMinutes(90)));
     }
 
     @Test
     public void shouldSerializeInHalfDays_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("HALF_DAYS");
-
-        Duration duration = Duration.ofDays(1);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "2", value);
+        assertEquals("2", mapper.writeValueAsString(Duration.ofDays(1)));
     }
 
     @Test
     public void shouldSerializeInHalfDaysDiscardingFractions_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("DAYS");
-
-        Duration duration = Duration.ofHours(30);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "1", value);
+        assertEquals("1", mapper.writeValueAsString(Duration.ofHours(30)));
     }
 
     @Test
     public void shouldSerializeInDays_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("DAYS");
-
-        Duration duration = Duration.ofDays(1);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "1", value);
+        assertEquals("1", mapper.writeValueAsString(Duration.ofDays(1)));
     }
 
     @Test
     public void shouldSerializeInDaysDiscardingFractions_whenSetAsPattern() throws Exception
     {
         ObjectMapper mapper = _mapperForPatternOverride("DAYS");
-
-        Duration duration = Duration.ofHours(36);
-        String value = mapper.writeValueAsString(duration);
-
-        assertEquals("The value is not correct.", "1", value);
+        assertEquals("1", mapper.writeValueAsString(Duration.ofHours(36)));
     }
 
     protected ObjectMapper _mapperForPatternOverride(String pattern) {
