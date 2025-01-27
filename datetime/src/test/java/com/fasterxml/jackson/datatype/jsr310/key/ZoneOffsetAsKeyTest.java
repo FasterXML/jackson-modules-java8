60fecb729875d64cec4b07562b7e89c720eb23f5
Kim, Joo Hyuk <beanskobe@gmail.com>
2025-01-27 21:05:14 +0900

    Revert "Undo `/key` tests to minimize changes"
    This reverts commit 541eb3b96520e8a33b3ed94b6985bb8ace96ea20.

--- datetime/src/test/java/com/fasterxml/jackson/datatype/jsr310/key/ZoneOffsetAsKeyTest.java
+++ datetime/src/test/java/com/fasterxml/jackson/datatype/jsr310/key/ZoneOffsetAsKeyTest.java
@@ -4,13 +4,14 @@ import java.time.ZoneOffset;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.ModuleTestBase;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ZoneOffsetAsKeyTest extends ModuleTestBase
 {
@@ -25,7 +26,7 @@ public class ZoneOffsetAsKeyTest extends ModuleTestBase
 
     private Map<ZoneOffset, String> map;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         map = new HashMap<>();
     }
@@ -36,7 +37,7 @@ public class ZoneOffsetAsKeyTest extends ModuleTestBase
 
         String value = MAPPER.writeValueAsString(map);
 
-        Assert.assertEquals("Value is incorrect", map(OFFSET_0_STRING, "test"), value);
+        assertEquals(map(OFFSET_0_STRING, "test"), value);
     }
 
     @Test
@@ -45,7 +46,7 @@ public class ZoneOffsetAsKeyTest extends ModuleTestBase
 
         String value = MAPPER.writeValueAsString(map);
 
-        Assert.assertEquals("Value is incorrect", map(OFFSET_1_STRING, "test"), value);
+        assertEquals(map(OFFSET_1_STRING, "test"), value);
     }
 
     @Test
@@ -53,7 +54,7 @@ public class ZoneOffsetAsKeyTest extends ModuleTestBase
         Map<ZoneOffset, String> value = MAPPER.readValue(map(OFFSET_0_STRING, "test"), TYPE_REF);
 
         map.put(OFFSET_0, "test");
-        Assert.assertEquals("Value is incorrect", map, value);
+        assertEquals(map, value);
     }
 
     @Test
@@ -61,7 +62,7 @@ public class ZoneOffsetAsKeyTest extends ModuleTestBase
         Map<ZoneOffset, String> value = MAPPER.readValue(map(OFFSET_1_STRING, "test"), TYPE_REF);
 
         map.put(OFFSET_1, "test");
-        Assert.assertEquals("Value is incorrect", map, value);
+        assertEquals(map, value);
     }
 
     private String map(String key, String value) {
